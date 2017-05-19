1aba429facce79b39d827672a5c9cc7466a8b9b2
Pablo Soler <pasoau@campusaula.com>
2017-05-19 10:46:24 +0200

    Actividad 7 JUnit: Contestadas todas las preguntas

--- EDE/JUnit/Actividad-7/src/com/practicas/ede/BinString.java
+++ EDE/JUnit/Actividad-7/src/com/practicas/ede/BinString.java
@@ -3,15 +3,18 @@
 package com.practicas.ede;
 
 // ¿Qué puede hacer esta Clase?
+// Esta clase contiene una serie de metodos que transforma una cadena de caracteres a binario
 public class BinString {
 	public BinString(){}
 
 	// ¿Qué hace este método?
+	// Realiza los metodos sumar y abinario
 	public String convertir(String s){
 		return aBinario(sumar(s));
 	}
 	
 	// ¿Qué hace este método?
+	// Transforma una cadena de caracteres en ascii
 	public int sumar(String s){
 		String s1 = s.substring(0);
 		int sum = 0;
@@ -25,6 +28,7 @@ public class BinString {
 	}
 	
 	// ¿Qué hace este método?
+	// Transforma un valor en binario
 	public String aBinario(int n){
 	    String s = "";
 	    
